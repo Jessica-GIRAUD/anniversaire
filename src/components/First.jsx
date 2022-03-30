1538f146c24110e3b54ad2abb22b1878e3f2ba0a
Jessica-GIRAUD <jessica.giraud@strateg.in>
2022-03-30 09:24:01 +0200

    new pic

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import "../App.css";
-import waze from "../images/brand_waze_icon_158625.png";
+import waze from "../images/waze.png";
 
 const First = () => {
   return (
     <div className="background">
-      <a
-        target="_blank"
-        className="waze"
-        href="https://ul.waze.com/ul?place=ChIJJTWkLEkmVQ0RiGZfEZjrtkk&ll=44.82544530%2C-0.55640740&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location"
-        rel="noreferrer"
-      >
-        <img src={waze} alt="waze" style={{ width: "100%" }} />
-        <p style={{ textAlign: "center", marginTop: 0 }}>
-          Clique ici pour découvrir ta destination...
-          <br />
-          <br />
-          Rendez-vous le 2 Avril à 10h30... SUR PLACE !
-        </p>
-      </a>
+      <div className="box">
+        <a
+          target="_blank"
+          href="https://ul.waze.com/ul?place=ChIJJTWkLEkmVQ0RiGZfEZjrtkk&ll=44.82544530%2C-0.55640740&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location"
+          rel="noreferrer"
+        >
+          <img src={waze} alt="waze" className="waze" />
+          <p>
+            Clique ici pour découvrir ta destination...
+            <br />
+            <br />
+            Rendez-vous le 2 Avril à 10h30... SUR PLACE !
+          </p>
+        </a>
+      </div>
     </div>
   );
 };
